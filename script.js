f1ebb08e1e7458b08613b30f66bd9590e48a4a26
“clar9309” <clar9309@stud.kea.dk>
2021-06-09 13:30:19 +0200

    burger menu

---
+++ script.js
@@ -0,0 +1,23 @@
+// Lytter til siden er loadet
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  console.log("toggleMenu");
+  // toggle betyder at jeg kan tilføje noget hvis det ikke er der, eller fjerne det hvis det er der
+  document.querySelector("#menu").classList.toggle("hidden");
+
+  //Skjuler burger menu
+  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+  //hvis den indeholder class hidden, og dt er sandt, så skjuler den det. Og hvis den ikke indeholder hidden, så er det falsk.
+  if (erSkjult == true) {
+    document.querySelector("#menuknap").textContent = "☰";
+  } else {
+    document.querySelector("#menuknap").textContent = "✕";
+  }
+}
